5631cf992be02652a8b642cd9af6b8aae06750ef
Sufian-786-mumtaz <sufianmumtaz786@gmail.com>
2021-08-20 23:00:52 +0500

    new

---
+++ Assests/js/Custom.js
@@ -0,0 +1,45 @@
+// var owl = $('.owl-carousel');
+// owl.owlCarousel({
+//     loop:true,
+//     nav:true,
+//     margin:10,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },            
+//         960:{
+//             items:5
+//         },
+//         1200:{
+//             items:6
+//         }
+//     }
+// });
+// owl.on('mousewheel', '.owl-stage', function (e) {
+//     if (e.deltaY>0) {
+//         owl.trigger('next.owl');
+//     } else {
+//         owl.trigger('prev.owl');
+//     }
+//     e.preventDefault();
+// });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
